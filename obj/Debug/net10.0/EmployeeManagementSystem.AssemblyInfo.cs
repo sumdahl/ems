6cebabff9f613f687a506cfbdea30861b0542c59
Sumiran Dahal <sumirandahal46@gmail.com>
2026-01-14 14:06:08 +0545

    feat: implemented user profile

--- obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d813dc371e31d1de247ad44eb9da2146841c98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58cd6d0922573e17ba03b04a44c66a65aa19a177")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
