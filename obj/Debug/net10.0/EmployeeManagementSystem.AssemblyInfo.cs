94054708986f0c6c78ded8388e93bd126985787c
Sumiran Dahal <sumirandahal46@gmail.com>
2026-01-19 21:34:25 +0545

    Update login and registration views, introduced eye toggle for password visibility.

--- obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3d578835605fc7bed8889decc7b2a39226d9f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ce0a6196f1c312bab4376291a609177857fafb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
