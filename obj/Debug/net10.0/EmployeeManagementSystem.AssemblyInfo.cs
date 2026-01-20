e647a8ffb759ceedc3ff98e7ef0c3264b2f50b3b
Sumiran Dahal <sumirandahal46@gmail.com>
2026-01-20 15:09:00 +0545

    build: Update project build artifacts.

--- obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93858d848c31025e29008b01d2cbd16877634049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54fb91266515c2cb9a8dc62f63938405f22c9597")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
